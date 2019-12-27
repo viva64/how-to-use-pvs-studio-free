012f5d0196dae14b1ef02a556f4e5d763432418a
retmas <retmas@users.noreply.github.com>
2019-12-28 02:31:14 +0300

    Convert to unsigned char arguments of isalnum(), etc.

--- comments.cpp
+++ comments.cpp
@@ -25,7 +25,7 @@ namespace PvsStudioFreeComments
   {
     size_t width = 0;
 
-    while (begin != end && !isalnum(*begin))
+    while (begin != end && !isalnum(static_cast<const unsigned char>(*begin)))
     {
       width += (*begin == '\t' ? 4 : (*begin == ' ' ? 1 : 0));
       ++begin;
@@ -36,7 +36,7 @@ namespace PvsStudioFreeComments
       return false;
     }
 
-    while (begin != end && isspace(*(end - 1)))
+    while (begin != end && isspace(static_cast<const unsigned char>(*(end - 1))))
     {
       --end;
     }
@@ -109,7 +109,7 @@ namespace PvsStudioFreeComments
 
   const char* CommentsParser::readComment(const char* it)
   {
-    while (isspace(*it) && m_skippedLines < MaxSkippedLines)
+    while (isspace(static_cast<const unsigned char>(*it)) && m_skippedLines < MaxSkippedLines)
     {
       if (it[0] == '\n')
         ++m_skippedLines;
