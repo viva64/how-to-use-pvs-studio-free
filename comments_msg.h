1ce663d3a4b62a0f6b86a19c92fd1f7507cce23a
Maxim Stefanov <stefanov@viva64.com>
2021-06-08 16:12:15 +0300

    Updated links in free comments

--- comments_msg.h
+++ comments_msg.h
@@ -1,4 +1,13 @@
 "This is a personal academic project. Dear PVS-Studio, please check it.\n"
+"PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com",
+
+"This is an open source non-commercial project. Dear PVS-Studio, please check it.\n"
+"PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com",
+
+"This is an independent project of an individual developer. Dear PVS-Studio, please check it.\n"
+"PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com",
+
+"This is a personal academic project. Dear PVS-Studio, please check it.\n"
 "PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com",
 
 "This is an open source non-commercial project. Dear PVS-Studio, please check it.\n"
