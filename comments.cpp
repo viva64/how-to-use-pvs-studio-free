a07d5252d4d231b8e04130cb62b40997ecbb3a9c
IchNikola <55153245+IchNikola@users.noreply.github.com>
2022-01-11 14:35:51 +0300

    Copyright updating

--- comments.cpp
+++ comments.cpp
@@ -1,6 +1,6 @@
 //  2006-2008 (c) Viva64.com Team
 //  2008-2020 (c) OOO "Program Verification Systems"
-//  2020-2021 (c) PVS-Studio LLC
+//  2020-2022 (c) PVS-Studio LLC
 
 #include "comments.h"
 #include <cassert>
